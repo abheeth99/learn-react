71c922b66c2c2bc947fc2a187e6550b3f140c8f0
abheeth99 <Abheethk@embla.asia>
2021-06-03 11:39:38 +0530

    plus minus with mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,79 @@
 import React from 'react';
 import './App.css';
 import { useLocalStore, useObserver } from 'mobx-react-lite';
+import { makeAutoObservable, makeObservable } from "mobx"
+import { observer } from "mobx-react"
+import ReactDOM from 'react-dom';
+import { action, observable } from 'mobx';
 
-type MyState = {
-  count: number;
+// type MyState = {
+//   count: number;
 
-}; 
-class App extends React.Component{
+// }; 
+// class App extends React.Component{
   
-  increment = (no: number) => {
-    this.setState((state : MyState) => ({
-      count: state.count + no,
-    }));
-  };
+//   increment = (no: number) => {
+//     this.setState((state : MyState) => ({
+//       count: state.count + no,
+//     }));
+//   };
 
-  decrement = (no: number) => {
-    this.setState((state : MyState) => ({
-      count: state.count - no,
-    }));
-  };
+//   decrement = (no: number) => {
+//     this.setState((state : MyState) => ({
+//       count: state.count - no,
+//     }));
+//   };
 
-  state: MyState = {
-    count: 0,
-  };
+//   state: MyState = {
+//     count: 0,
+//   };
+//   render() {
+//     return (
+//       <div> 
+//         <div>
+//           {this.state.count}
+//         </div>
+//         <button onClick={() => this.increment(1)}>Increase</button>
+//         <button onClick={() => this.decrement(1)}>Decrease</button>
+//       </div>
+//     );
+//   }
+// }
+
+class Counter {
+  count = 0
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  increase() {
+      this.count += 1
+  }
+
+  decrease() {
+      this.count -= 1
+  }
+}
+
+const newCounter = new Counter()
+
+const CounterView = observer(({ counter } : any) => (
+  <div> 
+    <div>
+      {counter.count}
+    </div>
+    <button onClick={() => counter.increase()}>Increase</button>
+    <button onClick={() => counter.decrease()}>Decrease</button>
+  </div>
+))
+
+class App extends React.Component{
+
   render() {
     return (
       <div> 
-        <div>
-          {this.state.count}
-        </div>
-        <button onClick={() => this.increment(1)}>Increase</button>
-        <button onClick={() => this.decrement(1)}>Decrease</button>
+        <CounterView counter={newCounter}></CounterView>
       </div>
     );
   }
