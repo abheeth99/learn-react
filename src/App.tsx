8c96ca5908728a4a4d3afd8264d07e8fb710825a
abheeth99 <Abheethk@embla.asia>
2021-06-03 11:20:49 +0530

    init plus minus using setState

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,38 @@ import React from 'react';
 import './App.css';
 import { useLocalStore, useObserver } from 'mobx-react-lite';
 
-function App() {
-  return (
-      <main>
-        box-master
-      </main>
-  );
+type MyState = {
+  count: number;
+
+}; 
+class App extends React.Component{
+  
+  increment = (no: number) => {
+    this.setState((state : MyState) => ({
+      count: state.count + no,
+    }));
+  };
+
+  decrement = (no: number) => {
+    this.setState((state : MyState) => ({
+      count: state.count - no,
+    }));
+  };
+
+  state: MyState = {
+    count: 0,
+  };
+  render() {
+    return (
+      <div> 
+        <div>
+          {this.state.count}
+        </div>
+        <button onClick={() => this.increment(1)}>Increase</button>
+        <button onClick={() => this.decrement(1)}>Decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
