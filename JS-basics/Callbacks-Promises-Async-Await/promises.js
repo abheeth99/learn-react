0e8a2d5fa2af165ee3b07e993d3dc3cfa5562982
abheeth99 <Abheethk@embla.asia>
2021-06-05 09:17:07 +0530

    promise all

--- JS-basics/Callbacks-Promises-Async-Await/promises.js
+++ JS-basics/Callbacks-Promises-Async-Await/promises.js
@@ -35,3 +35,35 @@ function createName(name) {
 createName({ firstName: "Sriwanth", lastName: "Kotelawala" })
     .then(getNames)
     .catch(err => console.log(err));
+
+
+// Promise all
+
+const promise1 = Promise.resolve(3);
+const promise2 = "Just a value";
+const promise3 = Promise.reject('Error!!!');
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3, promise4]).then((values) => {
+  console.log("Promise all with one rejection",values)
+}).catch(err => console.log(err));
+// console -> Error!!!
+
+// Note! If one promise gets rejected, then promise all will reject immediately.
+// To handle that we can handle each behaviour like this,
+
+Promise.all([
+    promise1.catch(err=>console.log(err)), 
+    promise2,
+    promise3.catch(err=>console.log(err)), 
+    promise4.catch(err=>console.log(err))]).then((values) => {
+    console.log("Promise all with one rejection",values)
+  });
+
+// console ->
+// 0: 3
+// 1: "Just a value"
+// 2: undefined
+// 3: "foo"
