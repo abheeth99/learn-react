c743feb94c2cbb8e15acc701be32a5b4b8dfc0ba
abheeth99 <Abheethk@embla.asia>
2021-06-07 11:11:19 +0530

    spread, rest, reduce

---
+++ JS-basics/Callbacks-Promises-Async-Await/higherOrderFunctions.js
@@ -0,0 +1,61 @@
+// let addFive = (number)=>{
+//     console.log("addFive",number+5);
+// }
+// addFive(5);
+
+// // Store function on a variable and call
+// let addFiveHolder = addFive;
+// addFiveHolder(10);
+
+const arrayOfNumbers = [1, 2, 3, 4, 5];
+
+// using Reduce
+function addNumbersWithReduce(numbers){
+    const sum = arrayOfNumbers.reduce((accumulator,
+        currentValue) => {
+            return accumulator + currentValue;
+        });
+        console.log("addNumbersWithReduce",sum);
+    }
+    
+
+addNumbersWithReduce(arrayOfNumbers);
+
+// using Arguments
+function addNumbersWithArgs(){
+    const sum = arguments[0].reduce((accumulator,
+        currentValue) => {
+            return accumulator + currentValue;
+        });
+        console.log("addNumbersWithArgs",sum);
+    }
+
+addNumbersWithArgs(arrayOfNumbers);
+
+// using rest
+function addNumbersWithRest(...numbers){ // gather the remaining parameters into an array
+    let sum = 0;
+    numbers.forEach(el=> {
+        sum  = sum += el;
+    });
+
+    console.log("addNumbersWithRest",sum);
+
+}
+addNumbersWithRest(1);
+addNumbersWithRest(1,2);
+addNumbersWithRest(1,2,3);
+
+
+// using spread
+function addNumbersWithSpread(...numbers){ // a,b,c,d,e or we can use ...numbers
+    let sum = 0;
+    numbers.forEach(el=> {
+        sum  = sum += el;
+    });
+
+    console.log("addNumbersWithSpread",sum);
+
+}
+addNumbersWithSpread(...arrayOfNumbers); // expands
+
